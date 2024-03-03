943a8f2e81f40054b2a29d2281bd2feb52d55ee9
Danielggz <dggz1993@gmail.com>
2024-03-03 09:48:52 +0000

    login functionality added. New html page for register

---
+++ js/login.js
@@ -0,0 +1,18 @@
+$("document").ready(function(){
+    $("#btnLogin").click(function(){
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        // Checks if the username or password are included in the FileList
+        if(username == "guest" && password == "abc123."){
+            window.location.href = "index.html"; 
+        }else{
+            // display error message if not valid
+            $("#errorMsg").html("<div class='alert alert-danger'>The username or password are not valid. Please try again</div>")
+        }
+
+    });
+    $("#btnRegister").click(function(){
+        window.location.href = "register.html"; 
+    });
+});
